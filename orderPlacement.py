1ff81e7b81203c60c14704b767f4f5d782aa012b
RajAherrao1810 <rajaherrao3002@gmail.com>
2024-09-27 12:41:43 +0530

    added stop loss order in orderPlacement

--- orderPlacement.py
+++ orderPlacement.py
@@ -42,3 +42,27 @@ class PlaceOrder:
             print(f"Order placed successfully. Order ID: {order_id}")
         except Exception as e:
             print(f"Order placement failed: {str(e)}")
+
+
+    @classmethod
+    def stopLossLimitOrder(cls, symbol, token, transaction_type, exchange, product_type, limit_price, trigger_price, quantity):
+        order_params = {
+            "variety": "NORMAL",  # Type of order
+            "tradingsymbol": symbol,  # Symbol (e.g., RELIANCE)
+            "symboltoken": token,  # Token for the symbol
+            "transactiontype": transaction_type,  # 'BUY' or 'SELL'
+            "exchange": exchange,  # 'NSE', 'BSE', etc.
+            "ordertype": 'STOPLOSS_LIMIT',  # Stop Loss Limit order
+            "producttype": product_type,  # 'CNC', 'INTRADAY', etc.
+            "duration": "DAY",  # Order validity duration
+            "price": limit_price,  # The limit price at which the order will execute
+            "triggerprice": trigger_price,  # The trigger price that activates the Stop Loss order
+            "quantity": quantity  # Number of shares/contracts
+        }
+        
+        try:
+            order_id = webSocket.smartApi.placeOrder(order_params)
+            print(f"Stop Loss Limit Order placed successfully. Order ID: {order_id}")
+        except Exception as e:
+            print(f"Stop Loss Limit Order placement failed: {str(e)}")
+
